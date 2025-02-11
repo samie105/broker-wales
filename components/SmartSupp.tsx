27d921e2a1644c8e6c9ee99a54d453791fa6d982
samie105 <samsonrichfield@gmail.com>
2025-02-11 11:44:44 +0100

    fgf

--- components/SmartSupp.tsx
+++ components/SmartSupp.tsx
@@ -1,35 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    smartsupp: any;
-    _smartsupp: any;
-  }
-}
+import Script from "next/scripts";
 
 export default function SmartSupp() {
-  useEffect(() => {
-    window._smartsupp = window._smartsupp || {};
-    window._smartsupp.key = "6e76ec9657790f47dd669287a359c8571d101661";
-    window.smartsupp =
-      window.smartsupp ||
-      function () {
-        (window.smartsupp._ = window.smartsupp._ || []).push(arguments);
-      };
-    window.smartsupp._ = window.smartsupp._ || [];
-
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://www.smartsuppchat.com/loader.js?";
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
-  return null;
+  return (
+    <Script
+      src="//code.jivosite.com/widget/7yYKStKUVH"
+      strategy="afterInteractive"
+      async
+    />
+  );
 }
